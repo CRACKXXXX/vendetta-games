df6143fa2642e4b7827747dd6e465c95c08113f9
Joel <joel@example.com>
2026-04-03 13:31:55 +0100

    chore: remove test scripts

--- check_img.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const b = fs.readFileSync('public/cursor-normal.png');
-console.log('normal:', b.readUInt32BE(16), 'x', b.readUInt32BE(20));
-const h = fs.readFileSync('public/cursor-hover.png');
-console.log('hover:', h.readUInt32BE(16), 'x', h.readUInt32BE(20));
